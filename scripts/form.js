992c5b560c4f15ea84b36f35c1c572f2efb84bf3
A-MadLad <adamjudd07@gmail.com>
2024-05-25 15:15:36 -0600

    option creation

--- scripts/form.js
+++ scripts/form.js
@@ -1,42 +1,52 @@
-
 // FOOTER
 
-window.addEventListener('load', () => {
-    const year = document.getElementById('currentyear');
-    year.innerHTML = new Date().getFullYear();
+window.addEventListener("load", () => {
+  const year = document.getElementById("currentyear");
+  year.innerHTML = new Date().getFullYear();
 });
 
-window.addEventListener('load', () => {
-    const modified = document.getElementById('lastModified');
-    modified.innerHTML = document.lastModified;
+window.addEventListener("load", () => {
+  const modified = document.getElementById("lastModified");
+  modified.innerHTML = document.lastModified;
 });
 
 // PRODUCT ARRAY
 
 const products = [
-    {
-      id: fc-1888,
-      name: "flux capacitor",
-      avg-rating: 4.5
-    },
-    {
-      id: fc-2050,
-      name: "power laces",
-      averagerating: 4.7
-    },
-    {
-      id: fs-1987,
-      name: "time circuits",
-      averagerating: 3.5
-    },
-    {
-      id: ac-2000,
-      name: "low voltage reactor",
-      averagerating: 3.9
-    },
-    {
-      id: jj-1969,
-      name: "warp equalizer",
-      averagerating: 5.0
-    }
-  ];
+  {
+    id: "fc - 1888",
+    name: "flux capacitor",
+    averagerating: 4.5,
+  },
+  {
+    id: "fc - 2050",
+    name: "power laces",
+    averagerating: 4.7,
+  },
+  {
+    id: "fs - 1987",
+    name: "time circuits",
+    averagerating: 3.5,
+  },
+  {
+    id: "ac - 2000",
+    name: "low voltage reactor",
+    averagerating: 3.9,
+  },
+  {
+    id: "jj - 1969",
+    name: "warp equalizer",
+    averagerating: 5.0,
+  },
+];
+
+function selectProduct() {
+  products.forEach(product => {
+    let item = document.createElement("option");
+    item.value = product.id;
+    item.textContent = product.name;
+    document.querySelector("#selectProduct").appendChild(item);
+  });
+}
+
+selectProduct();
