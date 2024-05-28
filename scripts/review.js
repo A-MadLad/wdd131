5a77e026503e540fa1b3bf7c76c79387612f6dd6
A-MadLad <adamjudd07@gmail.com>
2024-05-27 22:12:06 -0600

    fixed

--- scripts/review.js
+++ scripts/review.js
@@ -4,8 +4,6 @@ const reviewCount = document.querySelector(".reviewCount");
 
 let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
 
-numVisits = 1
-
 reviewCount.textContent = numVisits;
 
 numVisits++;
