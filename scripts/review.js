50aa06d9ba550a5573fe47be0bf416e5fa28c644
A-MadLad <adamjudd07@gmail.com>
2024-05-27 22:07:01 -0600

    finished form

--- scripts/review.js
+++ scripts/review.js
@@ -4,11 +4,7 @@ const reviewCount = document.querySelector(".reviewCount");
 
 let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
 
-if (numVisits !== 0) {
-	reviewCount.textContent = numVisits;
-} else {
-	reviewCount.textContent = `This is your first visit. 🥳 Welcome!`;
-}
+reviewCount.textContent = numVisits;
 
 numVisits++;
 
