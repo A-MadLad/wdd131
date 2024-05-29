7c396a8db66cd20709031410b28b79f45c8b6ef6
A-MadLad <adamjudd07@gmail.com>
2024-05-29 14:22:41 -0600

    completed things to do page

--- finalproject/scripts/alamo.js
+++ finalproject/scripts/alamo.js
@@ -8,6 +8,8 @@ window.addEventListener('load', () => {
     modified.innerHTML = document.lastModified;
 });
 
+// HAMBURGER BUTTON
+
 const mainnav = document.querySelector(".nav");
 const hambutton = document.querySelector("#menu");
 
@@ -15,3 +17,66 @@ hambutton.addEventListener("click", () => {
   mainnav.classList.toggle("show");
   hambutton.classList.toggle("show");
 });
+
+//THINGS TO DO CARDS
+
+const fun = [
+  {
+    placeName: "McGinn's PistachioLand",
+    imageUrl:
+      "./images/pl.jpg",
+    description: "McGinn's PistachioLand produces many award winning wines, pistachio candies, flavored pistachios and pistashico ice cream."
+  },
+  {
+    placeName: "White Sands National Park",
+    imageUrl: "./images/ws.jpg",
+    description: "Rising from the heart of the Tularosa Basin is one of the world's great natural wonders - the glistening white sands of New Mexico. Great wave-like dunes of gypsum sand have engulfed 275 square miles of desert, creating the world's largest gypsum dunefield. White Sands National Park preserves a major portion of this unique dunefield, along with the plants and animals that live here."
+  },
+  {
+    placeName: "Museum of Space History",
+    imageUrl:
+      "./images/sh.jpg",
+    description: "The New Mexico museum of space history."
+  },
+  {
+    placeName: "Alameda Park Zoo",
+    imageUrl:
+      "./images/ap.webp",
+    description: "It is home to nearly 200 animals representing 90 species. The 12-acre Alameda Park Zoo offers an education center, picnic area, playground, and gift shop that’s perfect for families and visitors of all ages. "
+  },
+  {
+    placeName: "Toy Train Depot",
+    imageUrl:
+      "./images/tt.jpg",
+    description: "The toy train depot is the most unique train museum you'll find anywhere. There is a little something for everyone; young, old, history buffs, train enthusiasts, south west adventurers, and souvenir seekers."
+  },
+  {
+    placeName: "Flickinger Center",
+    imageUrl:
+      "./images/fc.webp",
+    description: "The Flickinger Center for Performing Arts is amongst New Mexico’s most exciting and rapidly diversifying live performance venues. We are first and foremost a space where communities are built, emerging talent is nurtured and lives are enriched through creative connection. As a non-profit performing arts center, we strive to bring a variety of performance, education, and community programs and opportunities to the local, national and international stage. With local roots in the New Mexico art scene and a global reach hosting and collaborating with artists, musicians, and creatives from around the world, Flickinger needs to be the next step in your art journey."
+  },
+];
+
+funCard(fun);
+
+function funCard(fun) {
+  fun.forEach(item => {
+    let card = document.createElement("section");
+    let name = document.createElement("h3");
+    let img = document.createElement("img");
+    let p = document.createElement("p");
+
+    name.textContent = item.placeName;
+    img.setAttribute("src", item.imageUrl);
+    img.setAttribute("alt", `${item.placeName}`);
+    img.setAttribute("loading", "lazy");
+    p.textContent = item.description;
+
+    card.appendChild(name);
+    card.appendChild(img);
+    card.appendChild(p);
+
+    document.querySelector(".funGrid").appendChild(card);
+  });
+}
